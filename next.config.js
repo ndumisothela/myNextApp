b735a25c1a1ec20cdade931ce861afab104996a5
Ndumiso Thela <ndumisothela480@gmail.com>
2024-04-18 22:08:25 +0200

    redeploy change serverless

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-/** @type {import('next').NextConfig} */
+/*@type {import('next').NextConfig} */
 const nextConfig = {};
 
 module.exports = {
-  target: "serverless",
+  output: "standalone",
 };
 //module.exports = nextConfig
